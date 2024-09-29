a851173ac84f436770ecb426422f42a634897c1e
ChrisHashing <chriswindow466@gmail.com>
2024-09-30 05:03:50 +0530

    added more chains

--- templates/hardhat.config.js
+++ templates/hardhat.config.js
@@ -14,7 +14,31 @@ module.exports = {
     polygon_mainnet: {
       url: process.env.POLYGON_RPC_URL || "",
       accounts: [process.env.PRIVATE_KEY || ""]
+    },
+    arbitrum: {
+      url: process.env.ARBITRUM_RPC_URL || "",
+      accounts: [process.env.PRIVATE_KEY || ""]
+    },
+    blast: {
+      url: process.env.BLAST_RPC_URL || "",
+      accounts: [process.env.PRIVATE_KEY || ""]
+    },
+    mantle: {
+      url: process.env.MANTLE_RPC_URL || "",
+      accounts: [process.env.PRIVATE_KEY || ""]
+    },
+    optimism: {
+      url: process.env.OPTIMISM_RPC_URL || "",
+      accounts: [process.env.PRIVATE_KEY || ""]
+    },
+    zkSync: {
+      url: process.env.ZKSYNC_RPC_URL || "",
+      accounts: [process.env.PRIVATE_KEY || ""]
+    },
+    base: {
+      url: process.env.BASE_RPC_URL || "",
+      accounts: [process.env.PRIVATE_KEY || ""]
     }
   },
-  defaultNetwork: "polygon_mainnet"  // Ensure this is set to the correct network
+  defaultNetwork: "polygon_mainnet" // Set to the network you prefer as default
 };
